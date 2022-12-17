9a4e1830474195dd9bd8afad8a5b9c5da52ad1a3
Олександр <102046268+AlexElektrik@users.noreply.github.com>
2022-12-17 13:11:21 +0200

    виконав стилізацію

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,65 @@
-import React, {Component} from "react";
-import Statistics from "./Statistics";
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyles';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+import Section from './Section';
+import Statistics from './Statistics';
 
-export class App extends Component  {
-
+export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
-
-  goodFeedbackAdd = () =>
-    this.setState((prevState) => ({
-      good: prevState.good + 1
-    }));  
-  
-  neutralFeedbackAdd = () =>
-    this.setState((prevState) => ({
-      neutral: prevState.neutral + 1
-    })); 
-  
-  badFeedbackAdd = () =>
-    this.setState((prevState) => ({
-      bad: prevState.bad + 1
-      })); 
-  
-  countTotalFeedback = () => {
-    return this.state.good + this.state.bad + this.state.neutral
+    bad: 0,
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round(this.state.good/this.countTotalFeedback()*100)
-  }
-  
+  onLeaveFeedback = option =>
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + bad + neutral;
+  };
+
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
   render() {
     const { good, neutral, bad } = this.state;
     return (
-      <>
-        <h2>Please leave feedback</h2> 
-        <h2>Statistics</h2>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}/>
-          <button onClick={this.goodFeedbackAdd}>Good</button> 
-          <button onClick={this.neutralFeedbackAdd}>Neutral</button>
-          <button onClick={this.badFeedbackAdd}>Bad</button>
-        
-      </>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+        <GlobalStyle />
+      </div>
     );
+  }
 }
-};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StatisticsList, StatisticsSpan } from './StatisticsStyles';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <div>
-      <p>Good: <span>{good}</span></p>
-      <p>Neutral: <span>{neutral}</span></p>
-      <p>Bad: <span>{bad}</span></p>
-      <p>Total: <span>{total}</span></p>
-      <p>Positive feedback: <span>{positivePercentage}% </span></p>
-    </div>
-  );}
-    
-export default Statistics
+    <StatisticsList>
+      <p>
+        Good: <StatisticsSpan>{good}</StatisticsSpan>
+      </p>
+      <p>
+        Neutral: <StatisticsSpan>{neutral}</StatisticsSpan>
+      </p>
+      <p>
+        Bad: <StatisticsSpan>{bad}</StatisticsSpan>
+      </p>
+      <p>
+        Total: <StatisticsSpan>{total}</StatisticsSpan>
+      </p>
+      <p>
+        Positive feedback: <StatisticsSpan>{positivePercentage}% </StatisticsSpan>
+      </p>
+    </StatisticsList>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
+
+export default Statistics;
